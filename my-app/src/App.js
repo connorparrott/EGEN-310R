12a6ca9520e01cbc9c51a3d65f53c78669a095f0
Connor Parrott <connorparrott@montana.edu>
2023-06-05 10:21:31 -0600

    basic button and title as well as basic food gauge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Get the gauge element
+const gauge = document.querySelector('.gauge');
+
+// Get the amount element
+const amountElement = document.getElementById('amount');
+
+// Decrease the amount and update the gauge width
+function decreaseAmount() {
+    let amount = parseInt(amountElement.textContent);
+    if (amount > 0) {
+        amount--;
+        amountElement.textContent = amount;
+        gauge.style.width = amount + '%';
+    }
+}
+
+// Attach the decreaseAmount function to a button or any other event
+const decreaseButton = document.getElementById('decrease-button');
+decreaseButton.addEventListener('click', decreaseAmount);
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"Header"}>
+          <h1>Automated Pet Feeder</h1>
+      </div>
+
+        <div className={"Body"}>
+            <div className={"Preferences"}>
+            </div>
+            <div>
+                Food Remaining: {amountElement}
+            </div>
+            <div className="gauge-container">
+                <div className="gauge"></div>
+                <span className="gauge-label">Amount: <span id="amount">100</span></span>
+            </div>
+            <div>
+                <button id="decrease-button">Decrease</button>
+            </div>
+            <div>
+                <button className="Manual">Feed Now</button>
+            </div>
+            <div className={"Alarms"}>
+                Alarms and Alerts
+            </div>
+        </div>
     </div>
   );
 }
