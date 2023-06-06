993892a6692fa27358b10a5e9045c88d8c2d4e69
Connor Parrott <connorparrott@montana.edu>
2023-06-06 11:11:26 -0600

    updated alerts and created accurate and working capacity bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const amountElement = React.useRef(null);
+    const totalVolume = 150;
     const gauge = React.useRef(null);
+    const [foodRemaining, setFoodRemaining] = useState(totalVolume) //initialized at 100 oz storage
+    const [servingSize, setServingSize] = useState(15)  //initialized at 15 oz serving size
+    const [foodInterval, setFoodInterval] = useState(3) //initialized at 3 times per day being fed
 
-    function decreaseAmount() {
-        let amount = parseInt(amountElement.current.textContent);
-        if (amount > 0) {
-            amount--;
-            amountElement.current.textContent = amount;
-            gauge.current.style.width = amount + '%';
+
+    function lostConnection(){
+        //if connection to server
+        alert("Unable to connect to your pet feeder, please call (123) 456-7890 or send someone to check on your pet")
+    }
+    function manualFeed() {
+        if (foodRemaining > servingSize) {
+            setFoodRemaining(foodRemaining - servingSize)
+            gauge.current.style.width = ((foodRemaining - servingSize) / totalVolume * 100  ) + '%';
+            alert('Your pet has been fed ' + servingSize + "oz");
+        } else{
+            setFoodRemaining(0)
+            gauge.current.style.width = "0%"
+            alert("Your pet has run out of food and was not given a full serving. Your pet received " + foodRemaining + "oz")
         }
     }
 
-    function manualFeed() {
-        decreaseAmount();
-        alert('Your pet has been fed');
-    }
-
     function refillFood() {
-        let amount = 100; // Replace with actual sensor value
-        amountElement.current.textContent = amount;
-        gauge.current.style.width = amount + '%';
+        setFoodRemaining(totalVolume)
+        gauge.current.style.width = '100%';
     }
-
-    React.useEffect(() => {
-        const decreaseButton = document.getElementById('decrease-button');
-        const refillButton = document.getElementById('refill-button');
-
-        decreaseButton.addEventListener('click', decreaseAmount);
-        refillButton.addEventListener('click', refillFood);
-    }, []);
 
     return (
         <div className="App">
@@ -40,25 +37,25 @@ function App() {
             </div>
             <div className="Body">
                 <div className="Preferences"></div>
-                <div>Food Remaining: <span ref={amountElement}>100</span></div>
+                <div>Food Remaining: <span>{foodRemaining}</span></div>
+                <div>Current Serving Size: <span>{servingSize}</span></div>
                 <div className="gauge-container">
                     <div className="gauge" ref={gauge}></div>
-                    <span className="gauge-label">
-            Amount: <span id="amount">100</span>
+                    <span className="gauge-label"> Days Remaining with Current Serving Size <span id="amount"> {foodRemaining / servingSize} </span>
           </span>
                 </div>
-                <div>
-                    <button className="increment-button" id="refill-button">
-                        Refill
-                    </button>
-                    <button className={"decrease-button"} id={"decrease-button"}>Decrease</button>
-                </div>
+                    <label>Change Serving Size: </label><br/>
+                    <input className="inputField" type="text" id="serving"></input>
+                    <div>
+                        <button className="buttonTemp" id="refill-button" onClick={refillFood}>Refill</button>
+                        <button className="buttonTemp" id="lost-connection" onClick={lostConnection}>Lost Connection</button>
+                        <button className="buttonTemp" id="ChangeSS" onClick={()=> {setServingSize(document.getElementById("serving").value)}}>Change</button>
+                    </div>
                 <div>
                     <button className="button" id="manual-button" onClick={manualFeed}>
                         Feed Now
                     </button>
                 </div>
-                <div className="Alarms">Alarms and Alerts</div>
             </div>
         </div>
     );
